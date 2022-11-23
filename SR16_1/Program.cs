e58e698dcdff5924a18b16d99222901ee10ad0db
Юлия Волкова <volktul@gmail.com>
2022-11-23 23:58:13 +0300

    Добавьте файлы проекта.

---
+++ SR16_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace SR16_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Commodity[] commodities = new Commodity[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("Введите код");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите наименование");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                commodities[i] = new Commodity() { Code = code, Name = name, Price = price };
+            }
+            
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true,
+            };
+            string jsonString = JsonSerializer.Serialize(commodities, options);
+
+            using (StreamWriter sw = new StreamWriter("../../../Commodities.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
---
+++ SR16_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace SR16_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Commodities.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Commodity[] commodities = JsonSerializer.Deserialize<Commodity[]>(jsonString);
+
+            Commodity maxCommodity = commodities[0];
+            foreach (Commodity c in commodities)
+            {
+                if (c.Price > maxCommodity.Price)
+                {
+                    maxCommodity = c;
+                }
+            }
+            Console.WriteLine($"{maxCommodity.Name}");
+            Console.ReadKey();
+        }
+    }
+}
